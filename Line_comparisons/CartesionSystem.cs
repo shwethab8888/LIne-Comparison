7f2c92a5877f39c473334ebc6448fa6773c6506e
shwethab8888 <shwethab8888@gmail.com>
2023-02-27 21:34:06 +0530

    Line comparison sqrt problem

---
+++ Line_comparisons/CartesionSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_comparisons
+{
+    public class CartesionSystem
+    {
+        public static void Cartesian_System()
+        {
+
+            //co=ordinates
+            double x1 = 1.0;
+            double y1 = 2.0;
+            double x2 = 3.0;
+            double y2 = 4.0;
+
+            //calculate line length
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            // print result
+            Console.WriteLine("Lenth of line from ({0},{1}) to ({2},{3}) is {4}", x1, y1, x2, y2, length);
+        }
+    }
+}
---
+++ Line_comparisons/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Line_comparisons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Line comparisons");
+            CartesionSystem.Cartesian_System();
+
+        }
+    }
+}
